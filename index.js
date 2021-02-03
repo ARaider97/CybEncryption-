3f0e06082e57c947d27ebe2e29455016728e99d2
ARaider97 <78396871+ARaider97@users.noreply.github.com>
2021-02-03 15:04:56 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function Encrypt() {
+  const str = Document.getElementById(encrypt);
+  console.log("Recieved http request");
+  console.log("Splitting request");
+  Array.from(str);
+  
+  console.log("splitting ip for signature");
+  var ipsig = Document.getElementById(gfg);
+  console.log(ipsig);
+  
+}
