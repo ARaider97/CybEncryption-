cb51153d5c10d99987d250b1c2e07266ec956f6c
ARaider97 <78396871+ARaider97@users.noreply.github.com>
2021-02-03 17:03:33 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function Encrypt() {
       console.log("confirming https handshake");
-      var unENC = document.getElementById("encrypt");
+      var unENC = document.getElementById("inputE");
       window.alert(unENC);
 }
